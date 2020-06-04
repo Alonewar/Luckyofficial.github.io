19b8f0a681a6bbf09c43d8a45eb3c7580f59988b
Alonewar <66402036+Alonewar@users.noreply.github.com>
2020-06-04 18:38:31 +0530

    Added new files

---
+++ clockscript.js
@@ -0,0 +1,24 @@
+var hour=document.getElementById("hour");
+var minute=document.getElementById("minute");
+var second=document.getElementById("second");
+var digitalclock=document.getElementById("digitalclock");
+
+function setClockTime(){
+	const day = new Date();
+	const hh = day.getHours();//0-23
+	const mm = day.getMinutes();//0-59
+	const ss = day.getSeconds();//0-59
+	
+	const hourDeg = (hh*30)+(mm * 0.5);
+	const minuteDeg = (mm*6) + (ss * 0.1);
+	const secondDeg = (ss*6);
+	
+	hour.style.transform = `rotatez(${hourDeg}deg)`;
+	minute.style.transform = `rotatez(${minuteDeg}deg)`;
+	second.style.transform = `rotatez(${secondDeg}deg)`;
+	
+	
+	digitalclock.innerText=`${hh%12<10?'0':''}${hh % 12}:${mm<10?'0':''}${mm}${hh>12?'PM':'AM'}`;
+}setClockTime();
+
+setInterval(setClockTime,1000);
